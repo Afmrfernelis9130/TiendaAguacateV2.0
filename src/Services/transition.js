6cda29d0d8f7788ad4cfbb2880ef3b8dd92ef05b
Alexander <alexsancast@outlook.com>
2022-08-14 18:38:37 -0400

    Uniendo el login y el registro en una misma pagina

---
+++ src/Services/transition.js
@@ -0,0 +1,31 @@
+document.getElementById("btn-start-login").addEventListener("click",login);
+document.getElementById("btn-start-register").addEventListener("click",register);
+
+//var
+var containerForm = document.querySelector(".container-login-register");
+var formLogin = document.querySelector(".form-login");
+var formRegister = document.querySelector(".form-register");
+///
+var backBoxLogin = document.querySelector(".backlogin");
+var backBoxRegister = document.querySelector(".backregister");
+
+
+
+
+function login (){
+    formRegister.style.display ="none";
+    containerForm.style.left = "10px";
+    formLogin.style.display = "block";
+    backBoxRegister.style.opacity = "1";
+    backBoxLogin.style.opacity = "0";
+
+}
+
+function register (){
+    formRegister.style.display ="block";
+    containerForm.style.left = "410px";
+    formLogin.style.display = "none";
+    backBoxRegister.style.opacity = "0";
+    backBoxLogin.style.opacity = "1";
+
+}
